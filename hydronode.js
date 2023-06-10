60de475759962bad66670cf3b6f59dbaac72b482
Andy Foster <>
2023-06-10 11:14:59 -0400

    Add winter and lots of DRY tidyup

--- hydronode.js
+++ hydronode.js
@@ -8,7 +8,49 @@ const tous = {
   offpeak: 'OFFPEAK'
 }
 
-// TODO: Winter!
+function rate(now) {
+  const month = now.getMonth();
+  // const date = now.getDate();
+  const day = now.getDay();
+  const hour = now.getHours();
+
+  // Evening and weekend offpeak is the same in summer and winter
+  if (isWeekend(day) || isEvening(hour)) {
+    return tous.offpeak;
+  }
+
+  // In summer, the middle tier is onpeak
+  if (isSummer(month)) {
+    if (isMiddleTier(hour)) {
+      return tous.onpeak;
+    } else {
+      return tous.midpeak
+    }
+  } else { // Winter
+    if (isMiddleTier(hour)) {
+      return tous.midpeak;
+    } else {
+      return tous.onpeak
+    }
+  }
+}
+
+function isMiddleTier(hour) {
+  return hour >= 11 && hour < 17;
+}
+
+function isSummer(month) {
+  // Summer is May to October, months start from 0
+  return month >= 4 && month < 9;
+}
+
+function isWeekend(day) {
+  return day === 6 || day === 0;
+}
+
+function isEvening(hour) {
+  return hour < 7 || hour >= 19;
+}
 
 app.get('/', (req, res) => {
   res.send(rate(new Date()));
@@ -17,27 +59,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
-
-function rate(now) {
-  const day = now.getDay();
-  const hour = now.getHours();
-
-  // Weekend offpeak
-  if (day === 6 || day === 0) {
-    return tous.offpeak;
-  } else { // Weekdays
-    // Evening offpeak is 7pm to 7am
-    if (hour < 7 || hour >= 19) {
-      return tous.offpeak;
-    }
-    // Midpeak is 7-11am, 5-7pm
-    if ((hour >= 7 && hour < 11) || (hour >= 17 && hour < 19)) {
-      return tous.midpeak;
-    } else { // Must be onpeak
-      return tous.onpeak;
-    }
-  }
-}
 
 module.exports = {
   rate
--- hydronode.test.js
+++ hydronode.test.js
@@ -3,24 +3,47 @@ const expect = chai.expect;
 
 const hydronode = require('./hydronode.js');
 
-// Times are UTC
-const exampleSummerOnpeakTime = new Date('2023-06-09T16:30:00') // Friday at 4:30pm
-const exampleSummerMidpeakTime = new Date('2023-06-09T10:30:00') // Friday at 10:30am
-const exampleSummerOffpeakTime = new Date('2023-06-09T00:30:00') // Friday at 8:30pm
+// There is no timezone specified so these dates are what they are
+const exampleSummerOnpeakTime = new Date('2023-06-09T16:30:00') // A Friday in summer at 4:30pm
+const exampleSummerMidpeakTime = new Date('2023-06-09T10:30:00') // A Friday in summer at 10:30am
+const exampleSummerOffpeakTime = new Date('2023-06-09T20:30:00') // A Friday in summer at 8:30pm
+const exampleSummerWeekend = new Date('2023-06-11T12:30:00') // A Sunday in summer at 12:30pm
+
+const exampleWinterOnpeakTime = new Date('2023-12-06T10:30:00') // A Wednesday in winter at 10:30am
+const exampleWinterMidpeakTime = new Date('2023-12-06T16:30:00') // A Wednesday in winter at 4:30pm
+const exampleWinterOffpeakTime = new Date('2023-12-06T22:30:00') // A Wednesday in winter at 10:30pm
+const exampleWinterWeekend = new Date('2023-11-04T10:30:00') // A Saturday in winter at 10:30am
 
 describe('hydronode', () => {
+  var result;
+
   it('should say onpeak if onpeak', () => {
-    const result = hydronode.rate(exampleOnpeakTime);
+    result = hydronode.rate(exampleSummerOnpeakTime);
+    expect(result).to.equal('ONPEAK');
+
+    result = hydronode.rate(exampleWinterOnpeakTime);
     expect(result).to.equal('ONPEAK');
   });
 
   it('should say midpeak if midpeak', () => {
-    const result = hydronode.rate(exampleMidpeakTime);
+    result = hydronode.rate(exampleSummerMidpeakTime);
+    expect(result).to.equal('MIDPEAK');
+
+    result = hydronode.rate(exampleWinterMidpeakTime);
     expect(result).to.equal('MIDPEAK');
   });
 
   it('should say offpeak if offpeak', () => {
-    const result = hydronode.rate(exampleOffpeakTime);
+    result = hydronode.rate(exampleSummerOffpeakTime);
+    expect(result).to.equal('OFFPEAK');
+
+    result = hydronode.rate(exampleWinterOffpeakTime);
+    expect(result).to.equal('OFFPEAK');
+
+    result = hydronode.rate(exampleSummerWeekend);
+    expect(result).to.equal('OFFPEAK');
+
+    result = hydronode.rate(exampleWinterWeekend);
     expect(result).to.equal('OFFPEAK');
   });
 });
